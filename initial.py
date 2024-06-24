97ceea15d7bc3ae1fed2bcafa6c30002a93702fe
Brannen Taylor <brannen.taylor@ccm.com>
2024-06-24 07:02:31 -0400

    initial file

---
+++ initial.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+"""Initial file for depot - testing."""
+
+def main():
+    """Main is a runner function, used to call individual functions in order."""
+    pass
+
+
+############### START HERE #####################
+# if__name__ - will look into a hidden variable for the file, called 'dunder name', or 'double underscore name'.
+# if this script is being run directly, as a script, and not just a container of functions, the
+# if__name__ will run, to inspect the name of the script, which will be '__main__', and will evaluate to True.
+# if __name__ is True, then go set some settings, and then go run the 'main()' function.
+################################################
+if __name__ == "__main__":
+    main()
